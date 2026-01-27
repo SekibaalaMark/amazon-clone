1abd71b46b2425403fd69cd6711922927a6970bb
sekibaalamark <markmajje256@gmail.com>
2026-01-27 22:10:58 +0300

    made the cart value interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,8 +80,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         });
         }
 
+        let cartQuantity = 0;
+        cart.forEach((item)=>{
+            cartQuantity+=item.quantity;
+        })
 
-        console.log(cart);
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
         
     });
 });
